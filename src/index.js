c0dfb0f9386d9c04883a4f39e152cf1816a01038
Abhi-cal <iamabhimaity@gmail.com>
2023-02-21 01:27:34 +0530

    databash connected

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route');
-
+const { PrismaClient } = require('@prisma/client')
 
 const app = express();
 
@@ -10,6 +10,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', route)
 
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+   
+        await prisma.user.create({
+          data: {
+            name: 'Alice',
+            email: 'alice@prisma.io',
+           password:'AliceerDhon'
+           
+          },
+        })
+      
+        const allUsers = await prisma.user.findMany({
+          
+        })
+        console.dir(allUsers)
+      }
+ 
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
