2623259a9072cf03094ef97c3564dc6c8c434abd
Abhi-cal <iamabhimaity@gmail.com>
2023-02-20 22:12:33 +0530

    first commit

---
+++ src/controllers/userController.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql')
+
+exports.createuser = async (req,res) =>{
+
+}
+
+
+exports.getUser = async (req,res) =>{
+
+}
+
+exports.findUser = async (req,res) =>{
+
+}
+
+
+exports.updateUser = async (req,res) =>{
+
+}
+
+exports.deleteUser = async (req,res) =>{
+
+}
